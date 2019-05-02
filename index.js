94f6a005b57167815998b53d960119f5b1e198ca
Matthew Davila <matthew.davila@att.net>
2019-05-02 17:05:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ function dec(n){
   return n-=1
 }
 function makeInt(n){
-  parseInt('n', 10)
-    return parseInt()
+  return parseInt('n')
 }
 function preserveDecimal(n){
     return parseFloat('n')
